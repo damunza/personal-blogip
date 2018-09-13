db38f88a2e69bc91c0b94f71ec4f0e1ebd3ffe5c
damunza <danidamunza@gmail.com>
2018-09-13 16:01:13 +0300

    created a form for editing comments

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, SelectField
+from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Required
 
 class BlogForm(FlaskForm):
     blog = TextAreaField('Your Blog')
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    author = StringField('Author',validators=[Required()])
+    comment = TextAreaField('Your Comment')
     submit = SubmitField('Submit')
