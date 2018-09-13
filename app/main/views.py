5b415d5ce87979da28e73a214de4f2038bcfa591
damunza <danidamunza@gmail.com>
2018-09-13 15:59:48 +0300

    added routes to the comment and viewing the comment

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, redirect,url_for
 from flask_login import login_required, current_user
 from . import main
-from .forms import BlogForm
-from ..models import Blog
+from .forms import BlogForm, CommentForm
+from ..models import Blog, Comment
 
 @main.route('/')
 def index():
@@ -28,3 +28,29 @@ def new_blog():
 
    title = 'New Blog'
    return render_template('new_blog.html',title = title, blog_form = form)
+
+@main.route('/comment/<id>')
+def comment(id):
+    '''
+    function to return the pitches
+    '''
+    comment = Comment.get_comment(id)
+    print(comment)
+    title = 'comments'
+    return render_template('comment.html',title = title, comment = comment)
+
+@main.route('/new_comment/<int:id>', methods = ['GET', 'POST'])
+def new_comment(id):
+    form = CommentForm()
+
+    if form.validate_on_submit():
+        writer = form.author.data
+        com = form.comment.data
+
+        new_comment = Comment(comment = com, blog_id = id, author= writer)
+        new_comment.save_comment()
+
+        return redirect(url_for('main.index'))
+
+    title = 'New Comment'
+    return render_template('new_comment.html', title = title, comment_form = form, pitch_id = id)
