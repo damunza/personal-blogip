cb0bd6a69a83f96d75769114ea3d00ddc416bd21
damunza <danidamunza@gmail.com>
2018-09-16 18:43:04 +0300

    added gunicorn and flask-mail

--- config.py
+++ config.py
@@ -7,20 +7,40 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://daniel:dan15done@localhost/blog'
 
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Lets Blog!'
+    SENDER_EMAIL = 'dmndan124@gmail.com'
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
 
 class ProdConfig(Config):
     '''
     child class for production configurations
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://daniel:dan15done@localhost/blog'
 
 class DevConfig(Config):
     '''
     child class for development configurations
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://daniel:dan15done@localhost/blog'
+
     DEBUG = True
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test': TestConfig
 }
--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from app import create_app, db
-from app.models import User
+from app.models import User, Comment,Blog
 from flask_script import Manager,Server
 from flask_migrate import Migrate,MigrateCommand
 
@@ -14,7 +14,14 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db,User = User, Blog = Blog,Comment = Comment)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
     manager.run()
