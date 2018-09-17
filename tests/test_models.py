950ec1c849274c8101810ae003ce835ce94428de
damunza <danidamunza@gmail.com>
2018-09-17 09:26:04 +0300

    added a test for comment model

--- tests/test_models.py
+++ tests/test_models.py
@@ -16,24 +16,24 @@ class UserModelTest(unittest.TestCase):
     def test_password_verification(self):
         self.assertTrue(self.new_user.verify_password('blog'))
 
-# class CommentModelTest(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.new_comment = Comment(id = 1, comment = 'ha', author = 'me', blog_id = 1)
-#
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.new_comment,Comment))
-#
-#     def test_variables(self):
-#         self.assertEquals(self.new_comment.id,1)
-#         self.assertEquals(self.new_comment.comment, 'ha')
-#         self.assertEquals(self.new_comment.author, 'me')
-#         self.assertEquals(self.new_comment.blog_id, 1)
-#
-#     def test_get_comment(self):
-#         # self.new_comment.save_comment()
-#         self.get_comments = Comment.get_comment(1)
-#         self.assertEquals(self.get_comments, [] )
+class CommentModelTest(unittest.TestCase):
+
+    def setUp(self):
+        self.new_comment = Comment(id = 1, comment = 'ha', author = 'me', blog_id = 1)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_comment,Comment))
+
+    def test_variables(self):
+        self.assertEquals(self.new_comment.id,1)
+        self.assertEquals(self.new_comment.comment, 'ha')
+        self.assertEquals(self.new_comment.author, 'me')
+        self.assertEquals(self.new_comment.blog_id, 1)
+
+    def test_get_comment(self):
+        # self.new_comment.save_comment()
+        self.get_comments = Comment.get_comment(1)
+        self.assertEquals(self.get_comments, [] )
 
 # class BlogModelTest(unittest.TestCase):
 #
